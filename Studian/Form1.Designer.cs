e1046eef09ffa46dc72ef2052431b1961de54efb
Je66b <je66bgames@gmail.com>
2019-06-07 15:26:28 -0400

    added browser choice combo-box.. pushing to make sure i dont commit a revert like a dummy

--- Studian/Form1.Designer.cs
+++ Studian/Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -49,7 +51,7 @@
             // 
             // browse
             // 
-            this.browse.Location = new System.Drawing.Point(253, 157);
+            this.browse.Location = new System.Drawing.Point(253, 165);
             this.browse.Name = "browse";
             this.browse.Size = new System.Drawing.Size(75, 21);
             this.browse.TabIndex = 1;
@@ -59,7 +61,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(69, 157);
+            this.textBox1.Location = new System.Drawing.Point(69, 165);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(178, 20);
             this.textBox1.TabIndex = 2;
@@ -67,7 +69,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(69, 115);
+            this.textBox2.Location = new System.Drawing.Point(69, 122);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(158, 20);
             this.textBox2.TabIndex = 3;
@@ -92,11 +94,38 @@
             this.label2.TabIndex = 5;
             this.label2.Text = " ";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Chrome",
+            "Firefox",
+            "Internet Explorer",
+            "Edge",
+            "Safari"});
+            this.comboBox1.Location = new System.Drawing.Point(69, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Choose your browser...";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(524, 385);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
@@ -119,6 +148,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Studian/Form1.cs
+++ Studian/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,21 +20,22 @@ namespace Studian
             InitializeComponent();
         }
 
+        string procName = "";
+
         private void Button1_Click(object sender, EventArgs e)
         {
-            Rectangle bounds = this.Bounds;
-            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
-            {
-                using (Graphics g = Graphics.FromImage(bitmap))
-                {
-                    g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
-                }
-                string saveLocale = @textBox1.Text + @"\" + @textBox2.Text + ".jpeg";
-                bitmap.Save(saveLocale, ImageFormat.Jpeg);
-                label2.Text = textBox2.Text + " saved to " + textBox1.Text;
-            }
+            var proc = Process.GetProcessesByName(procName)[0];
+            IntPtr Hnd = proc.MainWindowHandle;
+            Bitmap screenShot = ScreenCap.PrintWindow(Hnd);
+            string saveLocale = @textBox1.Text + @"\" + @textBox2.Text + ".jpeg";
+            screenShot.Save(saveLocale, ImageFormat.Jpeg);
+
+            /*
+            string saveLocale = @textBox1.Text + @"\" + @textBox2.Text + ".jpeg";
+            bitmap.Save(saveLocale, ImageFormat.Jpeg);
+            label2.Text = textBox2.Text + " saved to " + textBox1.Text;
+            */
         }
-        //https://stackoverflow.com/questions/891345/get-a-screenshot-of-a-specific-application
         private void Button2_Click(object sender, EventArgs e)
         {
             //creates a folder browser dialogue box, fills chosen folder address into text box on form
@@ -46,5 +49,160 @@ namespace Studian
                 Environment.SpecialFolder root = folderDlg.RootFolder;
             }
         }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            procName = comboBox1.Text;
+        }
     }
+
+    public class ScreenCap
+    {
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        public static Bitmap PrintWindow(IntPtr hwnd)
+        {
+            GetWindowRect(hwnd, out RECT rc);
+
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, PixelFormat.Format24bppRgb);
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+
+            PrintWindow(hwnd, hdcBitmap, 0);
+
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+
+            return bmp;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        private int _Left;
+        private int _Top;
+        private int _Right;
+        private int _Bottom;
+
+        public RECT(RECT Rectangle) : this(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom)
+        {
+        }
+        public RECT(int Left, int Top, int Right, int Bottom)
+        {
+            _Left = Left;
+            _Top = Top;
+            _Right = Right;
+            _Bottom = Bottom;
+        }
+
+        public int X
+        {
+            get { return _Left; }
+            set { _Left = value; }
+        }
+        public int Y
+        {
+            get { return _Top; }
+            set { _Top = value; }
+        }
+        public int Left
+        {
+            get { return _Left; }
+            set { _Left = value; }
+        }
+        public int Top
+        {
+            get { return _Top; }
+            set { _Top = value; }
+        }
+        public int Right
+        {
+            get { return _Right; }
+            set { _Right = value; }
+        }
+        public int Bottom
+        {
+            get { return _Bottom; }
+            set { _Bottom = value; }
+        }
+        public int Height
+        {
+            get { return _Bottom - _Top; }
+            set { _Bottom = value + _Top; }
+        }
+        public int Width
+        {
+            get { return _Right - _Left; }
+            set { _Right = value + _Left; }
+        }
+        public Point Location
+        {
+            get { return new Point(Left, Top); }
+            set
+            {
+                _Left = value.X;
+                _Top = value.Y;
+            }
+        }
+        public Size Size
+        {
+            get { return new Size(Width, Height); }
+            set
+            {
+                _Right = value.Width + _Left;
+                _Bottom = value.Height + _Top;
+            }
+        }
+
+        public static implicit operator Rectangle(RECT Rectangle)
+        {
+            return new Rectangle(Rectangle.Left, Rectangle.Top, Rectangle.Width, Rectangle.Height);
+        }
+        public static implicit operator RECT(Rectangle Rectangle)
+        {
+            return new RECT(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom);
+        }
+        public static bool operator ==(RECT Rectangle1, RECT Rectangle2)
+        {
+            return Rectangle1.Equals(Rectangle2);
+        }
+        public static bool operator !=(RECT Rectangle1, RECT Rectangle2)
+        {
+            return !Rectangle1.Equals(Rectangle2);
+        }
+
+        public override string ToString()
+        {
+            return "{Left: " + _Left + "; " + "Top: " + _Top + "; Right: " + _Right + "; Bottom: " + _Bottom + "}";
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public bool Equals(RECT Rectangle)
+        {
+            return Rectangle.Left == _Left && Rectangle.Top == _Top && Rectangle.Right == _Right && Rectangle.Bottom == _Bottom;
+        }
+
+        public override bool Equals(object Object)
+        {
+            if (Object is RECT)
+            {
+                return Equals((RECT)Object);
+            }
+            else if (Object is Rectangle)
+            {
+                return Equals(new RECT((Rectangle)Object));
+            }
+
+            return false;
+        }
+    }
+
 }
